76f781b6507473b3b8103db8029ee53e3bfe25c8
firstctrl <hello@firstctrl.com>
2026-05-17 21:22:24 +0000

    fix: address review issues in monorepo scaffold (task 0.1)

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,17 @@
 // @ts-check
-const tseslint = require('@typescript-eslint/eslint-plugin');
-const tsParser = require('@typescript-eslint/parser');
+const tseslint = require('typescript-eslint');
+const js = require('@eslint/js');
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
-module.exports = [
+module.exports = tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    ignores: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/build/**',
-      '**/.next/**',
-      '**/.turbo/**',
-    ],
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tseslint,
-    },
     rules: {
-      ...tseslint.configs.recommended.rules,
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
   },
-];
+  {
+    ignores: ['**/dist/**', '**/.next/**', '**/node_modules/**'],
+  },
+);
