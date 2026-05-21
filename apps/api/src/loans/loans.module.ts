1fc269d8d7d7b183c9f8a3ceccc86e06f1258bf8
firstctrl <hello@firstctrl.com>
2026-05-21 17:33:15 +0000

    feat(loans): register DefaultRecoveryJob in LoansModule

--- apps/api/src/loans/loans.module.ts
+++ apps/api/src/loans/loans.module.ts
@@ -6,6 +6,7 @@ import { StaffLoansController } from './staff-loans.controller';
 import { LoansService } from './loans.service';
 import { LoansImportService } from './loans.import.service';
 import { OverdueDetectionJob } from './jobs/overdue-detection.job';
+import { DefaultRecoveryJob } from './jobs/default-recovery.job';
 import { Loan, LoanSchema } from './schemas/loan.schema';
 import { LoanRepayment, LoanRepaymentSchema } from './schemas/loan-repayment.schema';
 import { LoanImportBatch, LoanImportBatchSchema } from './schemas/loan-import-batch.schema';
@@ -29,7 +30,7 @@ import { ContributionsModule } from '../contributions/contributions.module';
     ContributionsModule,
   ],
   controllers: [LoansController, StaffLoansController],
-  providers: [LoansService, LoansImportService, OverdueDetectionJob, LoanScheduleSenderService],
+  providers: [LoansService, LoansImportService, OverdueDetectionJob, DefaultRecoveryJob, LoanScheduleSenderService],
   exports: [LoansService],
 })
 export class LoansModule {}
